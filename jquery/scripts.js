d8b3ad40d33a1b5d062bb7bece8b76c473e9f589
Japorized <johnson5756@live.com>
2015-09-22 17:28:24 +0800

    CV Update #5

--- jquery/scripts.js
+++ jquery/scripts.js
@@ -4,5 +4,20 @@ $(document).ready(function() {
 	$("img").lazyload({
 		effect: "fadeIn"
 	});
+
+	// CV Skills Tab
+	$(".skillstab ul li").click(function() {
+		if ( $(this).text() === "Development" ) {
+			$(this).siblings().removeClass("skill_active");
+			$(this).addClass("skill_active");
+			$(".outer_barContainer").fadeOut("fast");
+			$(".outer_pieContainer").delay(200).fadeIn("fast");
+		} else {
+			$(this).siblings().removeClass("skill_active");
+			$(this).addClass("skill_active");
+			$(".outer_pieContainer").fadeOut("fast");
+			$(".outer_barContainer").delay(200).fadeIn("fast");
+		}
+	});
 	
 });
