b98437517c90a7f7244bbdaddea7db7976629521
Japorized <johnson5756@live.com>
2015-10-30 09:40:49 +0800

    Fixing Visual Bugs 2

--- js/cv_page.js
+++ js/cv_page.js
@@ -2,14 +2,30 @@ $(document).ready(function() {
 
 	// CV Skills Tab
 	$(".skillstab .skill_tab").click(function() {
-		if ( $(this).text() === "Development" ) {
+		if ( $(this).hasClass('skill_dev') || $(this).hasClass('m_skill_dev') ) {
 			$(this).siblings().removeClass("skill_active");
-			$(this).addClass("skill_active");
+            
+            if ( $(this).hasClass('skill_dev') ) {
+                $(this).addClass("skill_active");
+                $(this).siblings(".m_skill_dev").addClass("skill_active");
+            } else {
+                $(this).addClass("skill_active");
+                $(this).siblings(".skill_dev").addClass("skill_active");
+            }
+            
 			$(".outer_barContainer").fadeOut("fast");
 			$(".outer_pieContainer").delay(200).fadeIn("fast");
 		} else {
 			$(this).siblings().removeClass("skill_active");
-			$(this).addClass("skill_active");
+            
+            if ( $(this).hasClass('skill_des') ) {
+                $(this).addClass("skill_active");
+                $(this).siblings(".m_skill_des").addClass("skill_active");
+            } else {
+                $(this).addClass("skill_active");
+                $(this).siblings(".skill_des").addClass("skill_active");
+            }
+            
 			$(".outer_pieContainer").fadeOut("fast");
 			$(".outer_barContainer").delay(200).fadeIn("fast");
 		}
