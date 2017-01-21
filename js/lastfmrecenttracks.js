ad928fec4d83b9d3f17b8649b69e69eb15672082
Japorized <johnson5756@live.com>
2017-01-21 17:15:18 -0500

    LastFM js fix attempt

--- js/lastfmrecenttracks.js
+++ js/lastfmrecenttracks.js
@@ -1,7 +1,7 @@
 function lfmMostRecentTrack(JSONdata) {
   var oTrack = (new Array().concat(JSONdata.recenttracks.track))[0];
   if(oTrack.image[3]["#text"] != "")
-    document.getElementById("lfmMostRecentTrackImage").src = oTrack.image[3]["#text"] ;
+    document.getElementById("lfmMostRecentTrackImage").src = oTrack.image[3]["#text"];
   if(oTrack.streamable == "1")
     document.getElementById("lfmMostRecentTrackPlayIcon").style.display = "inline";
   document.getElementById("lfmMostRecentTrackArtist").innerHTML = oTrack.artist["#text"];
